036e932afcc8f49e94ddfea362de17e66a563844
Seth Johnson <sethj@ubuntu.com>
2017-12-16 16:43:36 -0800

    Removed uneeded print/debug statements. Added mising close() calls. Improved logging.

--- alert.py
+++ alert.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 
 import urllib.request
@@ -24,11 +23,7 @@ def get_config():
         config = json.load(config_r)
         webhook_url = config['webhook_url']
         users = config['users']
-        print(type(users))
-        print("users are {}".format(users))
-        print("raw users are ---vegan--- {}".format(config['users']))
         lang = config['language']
-        # add any more config we need
 
 def send_discord(r_msg):
     data = {
@@ -38,40 +33,36 @@ def send_discord(r_msg):
 
 def update_data():
     global streak_data
-    print("Nothing is happening")
-    print(users)
     for user in users:
-        print(user)
-        with urllib.request.urlopen(api_endpoint + user) as data_r:
-            print(data_r.getcode())
-            data_p = json.loads(data_r.read().decode())
-            streak = get_streak(data_p)
-            print(streak)
-            streak_data[user] = streak
-            print(streak_data[user]) #DEBUG
-        #DEBUG print(data_p)
-        #DEBUG print(type(data_p))
+        try:
+            with urllib.request.urlopen(api_endpoint + user) as data_r:
+                data_p = json.loads(data_r.read().decode())
+        except Exception as e:
+            logging.exception("Failed to fetch or parse data for user {}. Skipping.".format(user))
+            logging.exception("Exception was: {}".format(e))
+            continue
+        streak = get_streak(data_p)
+        streak_data[user] = streak
 
 def update_data_file():
     try:
         streak_file = open("streak_data.json", 'w')
         streak_file.write(json.dumps(streak_data))
-        print("Bob: {}".format(json.dumps(streak_data)))
+        streak_file.close()
     except Exception as e:
-        logging.critical("the world has ended. data file cannot be accessed")
-        loggin.critical("Exception was {}".format(e))
+        logging.critical("Failed to open or write to data file. Aborting.")
+        logging.critical("Exception was {}".format(e))
 
 def check_data():
     #checks new data vs saved
     previous_r = open('streak_data.json')
     previous = json.load(previous_r)
+    previous_r.close()
     for user in previous.keys():
-        print("Loop 1: New: {} Old:{}".format(streak_data[user], previous[user]))
+        logging.INFO("Loop 1: New: {} Old:{}".format(streak_data[user], previous[user]))
         if streak_data[user] == previous[user]:
-            print("First If: New: {} Old:{}".format(streak_data[user], previous[user]))
+            logging.INFO("First If: New: {} Old:{}".format(streak_data[user], previous[user]))
         elif streak_data[user] > previous[user]:
-            print("Sending a message")
-            # send message
             if streak_data[user] > 1:
                 send_discord("@everyone {} has continued their streak of {} days! Congratulations!".format(user, streak_data[user]))
             elif streak_data[user] == 1:
@@ -79,29 +70,23 @@ def check_data():
             else:
                 send_discord("This message should not have been sent... *stratches head*. If recieved, call the president! Set DEFCON 1!")
                 logging.critical("WTH just happened")
-    update_data_file()
 
 def get_streak(data_p):
-    #DEBUG print(type(data_p))
     streak = data_p["language_data"][lang]["streak"]
-    #DEBUG print(streak)
     return streak
 
 
 def main():
     # check if existing saved data
-    print("In main") #DEBUG
-    get_config()
+    try:
+        get_config()
+    except Exception as e:
+        logging.critical("Failed to load configuration. Aborting.")
+        logging.critical("Full error is: {}".format(e))
     update_data()
     if os.path.exists('streak_data.json'):
-        #DEBUG print("it worked?")
-        # call check_data()
         check_data()
-    else:
-        print("In except") #DEBUG
-        # create data file
-        update_data_file()
-    #DEBUG print(streak_data.keys())
+    update_data_file()
 
 
 main()
