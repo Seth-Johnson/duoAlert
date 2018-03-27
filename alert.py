60e839041096dd8e00186af142f04866d79cbd98
Kevin S <hi@sirbomble.com>
2018-03-26 21:19:42 -0700

    Added Phrases
    Added Randomized Phrase and Gifs to Streaks

--- alert.py
+++ alert.py
@@ -7,17 +7,25 @@ import json
 import logging
 import time
 import datetime
+import random
 
 api_endpoint = 'http://www.duolingo.com/users/'
 
 webhook_url = None
 users = []
 streak_data = {}
-version = "0.2"
+version = "0.4"
+phrase_r = {}
 
 timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
 complete_timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 logging.basicConfig(filename="duoAlert.log", level=logging.INFO)
+
+def get_phrase():
+    with open('phrases.json') as phrase_r:
+        phrase = json.load(phrase_r)
+        phrases = phrase['phrases']
+        return random.choice(phrases)
 
 def get_config():
     global users
@@ -28,14 +36,20 @@ def get_config():
         users = config['users']
         logging.info("Config set.")
 
-def send_discord(r_msg):
+def send_discord(r_msg, url = None):
+    if url is None:
+        url = ""
     data = {
         "embeds":[{
           "title":"Duolingo Streak Bot",
           "description":"{}".format(r_msg),
+          "color":0xff8000,
           "type":"rich",
           "thumbnail": {
             "url":"https://i.imgur.com/OTFSldg.png"
+          },
+          "image": {
+            "url":"{}".format(url)
           },
           "footer":{
             "text":"DuoAlert v{} | {}".format(version, timestamp),
@@ -46,7 +60,6 @@ def send_discord(r_msg):
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     r = requests.post(webhook_url, data=json.dumps(data), headers=headers)
     logging.info("Post data: {}".format(r))
-
 def update_data():
     global streak_data
     for user in users:
@@ -77,23 +90,25 @@ def check_data():
     previous_r.close()
     logging.info("Loaded streak data.")
     for user in previous.keys():
+        ph = get_phrase()
+        phtext = ph["text"]
+        phurl = ph["url"]
         logging.info("Loop 1 for {}: New: {} Old:{}".format(user, streak_data[user], previous[user]))
         if streak_data[user] == previous[user]:
             logging.info("No new streak for {}. Skipping.".format(user))
         elif streak_data[user] > previous[user]:
             if streak_data[user] > 1:
-                send_discord("@everyone {} has continued their streak of {} days! Congratulations!".format(user, streak_data[user]))
+                send_discord("@everyone {} has continued their streak of {} days! {}!".format(user, streak_data[user], phtext, phurl)
                 logging.info("{} has extended their streak.".format(user))
             elif streak_data[user] == 1:
                 send_discord("@everyone {} has restarted their streak! Clap with pity.".format(user))
-                logging.info("{} restarted their streak".format(user))
+                logging.ingo("{} restarted their streak".format(user))
             else:
                 send_discord("This message should not have been sent... *stratches head*. If recieved, call the president! Set DEFCON 1!")
                 logging.critical("WTH just happened")
         elif streak_data[user] is 0 and previous[user] > 0:
             send_discord("@everyone {} has lost their streak! Tease them mercilessly.".format(user))
             logging.info("{} failed their streak. Loser.".format(user))
-
 def get_streak(data_p):
     streak = data_p["site_streak"]
     return streak
