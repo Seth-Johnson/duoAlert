6815fa9a92118f3dd440718a77bd8d7a0507d94e
parkcitymedia <blakemichaelgaynor@gmail.com>
2021-05-11 12:40:46 -0700

    scaffold get_config()

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,57 @@ extern crate serde;
 extern crate serde_derive;
 extern crate reqwest;
 
-use rand::Rng;
+//use rand::Rng;
 use std::path::Path;
-//use error_chain::error_chain;
-use std::io::Read;
+use std::fs::File;
+//use std::io::Read;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
-//use reqwest::Client;
+use serde_json::{Value, Map, Result};
 
-// global structure definitions
+// Holds the config data
+struct Config {
+    users: String,
+    webhook_url: String,
+    giphy_apikey: String,
+    giphy_rating: String,
+    username: String,
+    password: String
+}
 
-    // Holds the config data
-    struct Config {
-        duo_username: String,
-        duo_password: String
+fn get_config(cfg_path: &str) {
+
+    if !Path::new(cfg_path).exists() {
+        
+        // cry about nonexistent path
+        println!("failed to retrieve config file from {}", String::from(cfg_path));
+
+    } else {
+        
+        // check the config data in the file
+        
     }
+}
 
-fn get_config(cfg_path: String) {
-    //
+fn send_discord(r_msg: String, url :String, version: String, timestamp: String ) {
+    
+    let data = (r#" {
+        "embeds":[{
+          "title":"Oxide Alert",
+          "description":"{}",
+          "color":0xff8000,
+          "type":"rich",
+          "thumbnail": {
+            "url":"https://cdn.discordapp.com/attachments/722708774967574618/841396425429352488/68747470733a2f2f692e696d6775722e636f6d2f68534c30784b502e706e67-NEW-icon.png"
+          },
+          "image": {
+            "url":"{}"
+          },
+          "footer":{
+            "text":"DuoAlert v{} | {} | Powered by GIPHY",
+            "icon_url":"https://cdn.discordapp.com/attachments/722708774967574618/841396425429352488/68747470733a2f2f692e696d6775722e636f6d2f68534c30784b502e706e67-NEW-icon.png"
+          }
+        }]
+    }"#, r_msg, url, version, timestamp);
 }
 
 fn login(logindata: Config) {
@@ -36,28 +69,36 @@ fn update_data() {
 }
 
 fn check_data() {
+    let previous_r = File::open("streak_data.json");
+    //let previous = /*json load */(previous_r);
 
 }
 
 fn update_data_file() {
-
+    /*
+    Dump json to streak_data.json
+    */
 }
 
 
 fn main() {
 
-    let config_path: String = String::from("config.json");
+    let config_path: &str = "config.json";
     /*
     The config loader thing
     */
-    let my_login = Config {
-        duo_username: String::from("myUserName"),
-        duo_password: String::from("myPassword")
+    let my_config = Config {
+        users: String::from(""),
+        webhook_url: String::from(""),
+        giphy_apikey: String::from(""),
+        giphy_rating: String::from(""),
+        username: String::from(""),
+        password: String::from("")
     };
 
-    get_config(String::from(config_path));
+    get_config(config_path);
 
-    login(my_login);
+    login(my_config);
 
     update_data();
 
